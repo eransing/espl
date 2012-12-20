e13ab7babe55a84b2d34c6ac5979946277140261
eransing <eransing@post.bgi.ac.il>
2012-12-20 11:56:07 +0200

    lab7 works

--- lab6/bcmpa.asm
+++ lab6/bcmpa.asm
@@ -1,5 +1,4 @@
-%include "io.mac"
-.CODE
+section .CODE
 	global main
 	extern old_main
 
@@ -10,7 +9,8 @@ main:
       mov	edx, DWORD[ebp+8]	; save the argv[2] addres
 	  call	open				;open both files and save the contant to ecx
 	  call	read
-	  mov	ebx,0
+	  mov	ebx,0 ;y?
+	  mov BX, placeOfChar
 	  jmp	diffloop
 	 
 	 ; push	ecx
@@ -22,22 +22,21 @@ main:
 	  
 read:
 	mov eax,3 ; linux system call- read
-    mov ecx,[esp+8]
-    mov edx,[esp+12]
+    ;mov ecx,[esp+8]
+    ;mov edx,[esp+12]
 	int 0x80	;execute the system call 
 	ret
 	
 write: ; i think it;s not in use
-	mov eax,4 ; linux system call- write
-    mov ecx,[esp+8]
-    mov edx,[esp+12]
-	int 0x80	;execute the system call 
+	mov	ebx,1	;file descriptor (stdout)
+	mov	eax,4	;system call number (sys_write)
+	int	0x80	;call kernel
 	ret
 	
 open:
 	mov eax,5; linux system call- open
-    mov ecx,[esp+8]
-    mov edx,[esp+12]
+    ;mov ecx,[esp+8]
+    ;mov edx,[esp+12]
 	int 0x80	;execute the system call 
 	ret
 	
@@ -50,16 +49,16 @@ close:
 
 	
 diffloop:
-	inc		placeOfChar
+	inc		BX
 	movzx	AX, BYTE [ecx]		; take one byte each time to AX	
-	movzx	BX, BYTE [edx]		; take one byte each time to BX
+	movzx	AL, BYTE [edx]		; take one byte each time to BX
 	cmp	AX, 0 		; if ( str1[i] == 0 ) break;
 	je	end
-	cmp	BX, 0 		; if ( str2[i] == 0 ) break;
+	cmp	AL, 0 		; if ( str2[i] == 0 ) break;
 	je	end
-	cmp AX,BX		;if not equal write the line
-	jne printline
-	je	continue
+	cmp AX,AL		
+	jne printline	;if not equal write the line
+	je	continue	;else continue
 	
 continue:	
 	inc	ecx
@@ -67,13 +66,25 @@ continue:
 	jmp diffloop
 
 printline:
-	PutStr msgSec1
-	PutInt placeOfChar
-	PutStr msgSec2
-	PutInt AX
-	PutStr msgSec3
-	PutInt BX
-	nwln
+;clean the registers
+;go to func
+; retrive registers
+	mov	edx,len1	;message length
+	mov	ecx,msgSec1	;message to write
+	call write
+	mov	ecx,placeOfChar
+	call write
+	
+	mov	edx,len2	;message length
+	mov	ecx,msgSec2	;message to write
+	call write
+	mov	ecx,AX  ;______________invalid
+	call write
+	mov	edx,len3	;message length
+	mov	ecx,msgSec3	;message to write
+	mov	ecx,BX		;______________invalid
+	call write
+	;nwln
 	jmp continue
 
 	
@@ -82,10 +93,13 @@ end:
 	pop ebp
 	ret
 	
-.DATA
-		placeOfChar Dw 0
+section .data
+		placeOfChar Db 1
 		msgSec1		Db 'byte ','\0'
+		len1	equ	$ - msgSec1	       	 ;length of our dear string
 		msgSec2		Db ' -','\0'
+		len2	equ	$ - msgSec2	       	 ;length of our dear string
 		msgSec3		Db ' +','\0'
+		len3	equ	$ - msgSec3	       	 ;length of our dear string
 		
 		
---
+++ lab7/bkup.sh~
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# the backup ..
+
+
+fileName=$1
+NumOfVersion=$2
+
+
+#check if file exist
+if [ ! -e $fileName ] ; then
+  echo "$0: Missing file $fileName" >&2
+  echo "try again with an existing file" >&2
+  exit 1
+fi
+
+#copy the contant of the file to the new version
+cp $fileName newVersion.txt
+
+echo "everything is ok till now"
+
+
+
+while [ "$NumOfVersion" != "1" ]; do
+
+if [ -e $fileName"."$(( $NumOfVersion - 1)) ] ; then
+
+cp $fileName"."$(( $NumOfVersion - 1)) $fileName"."$NumOfVersion 
+fi
+
+NumOfVersion="$(( $NumOfVersion - 1 ))"
+echo "$NumOfVersion"
+
+done
+
+cp newVersion.txt $fileName".1"
+
+rm newVersion.txt
+
+exit 0
